c8137b42048000737c88200d12ad28cc326173a6
Kurt Ronshausen <kurt@agrian.com>
2022-10-07 09:52:47 -0700

    DEV-36098 - pr cleanup

--- spec/unit/ogr/geometry/rttopo_extensions_spec.rb
+++ spec/unit/ogr/geometry/rttopo_extensions_spec.rb
@@ -31,9 +31,9 @@ RSpec.describe OGR::Geometry do
       include_context 'crossing lines'
 
       it 'makes a valid MULTIPOLYGON' do
-        result_wkt = 'MULTIPOLYGON (((10 0,5 0,5 5,10 0)),' \
-                     '((3.33333333333333 6.66666666666667,0 0,0 10,3.33333333333333 6.66666666666667)),' \
-                     '((5 10,5 5,3.33333333333333 6.66666666666667,5 10)))'
+        result_wkt = 'MULTIPOLYGON (((0 0,0 10,3.33333333333333 6.66666666666667,0 0)),' \
+                     '((5 5,10 0,5 0,5 5)),' \
+                     '((5 5,3.33333333333333 6.66666666666667,5 10,5 5)))'
 
         expect(subject.make_valid.to_wkt).to eq(result_wkt)
       end
